e7299e023f70452671b0604f75fe0e6166e2226b
David Lorca <david.lorcacortes@cencosud.cl>
2019-08-05 22:38:21 -0400

    Se crea función de promedio

---
+++ desafio7/velocidad.py
@@ -0,0 +1,16 @@
+def promedio(velocidad):
+    suma=0
+    for pos, value in enumerate(velocidad):
+        suma = suma + value
+    return (suma/len(velocidad))
+
+
+velocidad = [4, 4, 7, 7, 8, 9, 10, 10, 10,
+            11, 11, 12, 12, 12, 12, 13, 13,
+            13, 13, 14, 14, 14, 14, 15, 15,
+            15, 16, 16, 17, 17, 17, 18, 18,
+            18, 18, 19, 19, 19, 20, 20, 20,
+            20, 20, 22, 23, 24, 24, 24, 24, 25]
+
+pepe=promedio(velocidad)
+print(pepe)
